ae37c6fdbed400097277b0e9a58790f66a10cf60
Cems <36056008+cdricms@users.noreply.github.com>
2022-08-15 16:35:14 +0200

    Preview graph inside the modal. (#26)
    * Can now inserts plots directly from a modal
    
    * Issues reguarding undefined values solved + Better naming import
    
    * Preview inside the modal
    
    * Removed manual bump

--- src/PlotFunctionModal.ts
+++ src/PlotFunctionModal.ts
@@ -1,5 +1,31 @@
 import { App, Modal, Setting } from 'obsidian'
+import { plotFunction } from './main'
 import { HeaderOptions } from './types'
+
+export function parseToPlot(
+  result: HeaderOptions & { functions: string }
+): string {
+  return `
+\`\`\`functionplot
+${parseToYAML(result)}
+\`\`\`
+`
+}
+
+export function parseToYAML(
+  result: HeaderOptions & { functions: string }
+): string {
+  return `---
+title: ${result.title ?? "''"}
+disableZoom: ${result.disableZoom}
+bounds: [${result.bounds}]
+grid: ${result.grid}
+xLabel: ${result.xLabel ?? "''"} 
+yLabel: ${result.yLabel ?? "''"}
+---
+${result.functions ?? ''}
+`
+}
 
 export default class PlotFunctionModal extends Modal {
   title: HeaderOptions['title']
@@ -21,20 +47,22 @@ export default class PlotFunctionModal extends Modal {
     this.onSubmit = onSubmit
   }
 
-  static parseToPlot(result: HeaderOptions & { functions: string }): string {
-    return `
-\`\`\`functionplot
----
-title: ${result.title ?? "''"}
-disableZoom: ${result.disableZoom}
-bounds: [${result.bounds}]
-grid: ${result.grid}
-xLabel: ${result.xLabel ?? "''"}
-yLabel: ${result.yLabel ?? "''"}
----
-${result.functions ?? ''}
-\`\`\`
-`
+  reloadPreview(previewDiv: HTMLElement) {
+    while (previewDiv.firstChild) {
+      previewDiv.removeChild(previewDiv.firstChild)
+    }
+    plotFunction(
+      parseToYAML({
+        title: this.title,
+        xLabel: this.xLabel,
+        yLabel: this.yLabel,
+        bounds: this.bounds,
+        disableZoom: this.disableZoom,
+        grid: this.grid,
+        functions: this.functions,
+      }),
+      previewDiv
+    )
   }
 
   onOpen(): void {
@@ -43,24 +71,35 @@ ${result.functions ?? ''}
     // Header
     contentEl.createEl('h1', { text: 'Plot a function.' })
 
-    new Setting(contentEl).setName('Title').addText((text) => {
+    const flex = contentEl.createDiv({ cls: 'flex' })
+    flex.style.display = 'flex'
+    flex.style.alignItems = 'center'
+
+    const settings = flex.createDiv({ cls: 'settings' })
+    const preview = flex.createDiv()
+    this.reloadPreview(preview)
+
+    new Setting(settings).setName('Title').addText((text) => {
       text.onChange((value) => {
         this.title = value
+        this.reloadPreview(preview)
       })
     })
 
-    new Setting(contentEl).setName('X').addText((text) => {
+    new Setting(settings).setName('X').addText((text) => {
       text.onChange((value) => {
         this.xLabel = value
+        this.reloadPreview(preview)
       })
     })
-    new Setting(contentEl).setName('Y').addText((text) => {
+    new Setting(settings).setName('Y').addText((text) => {
       text.onChange((value) => {
         this.yLabel = value
+        this.reloadPreview(preview)
       })
     })
 
-    new Setting(contentEl)
+    new Setting(settings)
       .setName('Bounds')
       .setDesc('Bounds must be written in this format: minX, maxX, minY, maxY')
       .addText((text) => {
@@ -70,30 +109,37 @@ ${result.functions ?? ''}
             .getValue()
             .split(',')
             .map((c) => parseFloat(c))
-          if (bounds.length === 4)
+          if (bounds.length === 4) {
             this.bounds = bounds as HeaderOptions['bounds']
+            this.reloadPreview(preview)
+          }
         }
       })
 
-    new Setting(contentEl).setName('Disable Zoom').addToggle((com) => {
+    new Setting(settings).setName('Disable Zoom').addToggle((com) => {
       com.setValue(this.disableZoom)
       com.onChange((value) => {
         this.disableZoom = value
+        this.reloadPreview(preview)
       })
     })
 
-    new Setting(contentEl).setName('Enable Grid').addToggle((com) => {
+    new Setting(settings).setName('Enable Grid').addToggle((com) => {
       com.setValue(this.grid)
       com.onChange((value) => {
         this.grid = value
+        this.reloadPreview(preview)
       })
     })
 
-    new Setting(contentEl).setName('Functions').addTextArea((com) => {
+    new Setting(settings).setName('Functions').addTextArea((com) => {
       com.onChange((value) => {
         this.functions = value
       })
+      com.inputEl.onchange = (_) => this.reloadPreview(preview)
     })
+
+    // Preview
 
     new Setting(contentEl).addButton((btn) => {
       btn
--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import {
   Editor,
 } from 'obsidian'
 
-import PlotFunctionModal from './PlotFunctionModal'
+import PlotFunctionModal, { parseToPlot } from './PlotFunctionModal'
 import { HeaderOptions } from './types'
 
 const DEFAULT_HEADER_OPTIONS: HeaderOptions = {
@@ -19,6 +19,61 @@ const DEFAULT_HEADER_OPTIONS: HeaderOptions = {
   yLabel: '',
 }
 
+export async function plotFunction(
+  source: string,
+  el: HTMLElement
+): Promise<void> {
+  try {
+    // styles
+    el.classList.add('obsidian-functionplot-render')
+    // parse yaml for bounds and functions to plot
+
+    const header = (source.match(/-{3}[^]+-{3}/) || [null])[0]
+    const functions = (header ? source.substring(header.length) : source)
+      .split('\n')
+      .map((line) => line.trim())
+      .filter((line) => line.length > 0)
+
+    const config: HeaderOptions = Object.assign(
+      {},
+      DEFAULT_HEADER_OPTIONS,
+      header ? parseYaml(header.match(/-{3,}([^]+?)-{3,}/)[1] || '') : {}
+    )
+
+    // prepare options for call to FunctionPlotya
+    const fPlotOptions: FunctionPlotOptions = {
+      target: el,
+      title: config.title,
+      grid: config.grid,
+      disableZoom: config.disableZoom,
+      xAxis: {
+        domain: [config.bounds[0], config.bounds[1]],
+        label: config.xLabel,
+      },
+      yAxis: {
+        domain: [config.bounds[2], config.bounds[3]],
+        label: config.yLabel,
+      },
+      data: functions.map((line) => {
+        return { fn: line.split('=')[1].trim(), graphType: 'polyline' }
+      }),
+    }
+    // render
+    functionPlot(fPlotOptions)
+    // make text listen to stylesheet
+    el.querySelectorAll('text').forEach((el) =>
+      el.setAttribute('fill', 'currentColor')
+    )
+  } catch (e) {
+    el.innerHTML = `
+    <div class="obsidian-functionplot-error">
+      <h3>Error</h3>
+      <p>${e}\n\n${source}</p>
+      <p>Please check your syntax. If this error keeps happening, please file a bug report on <a href="https://github.com/leonhma/obsidian-functionplot#bugs-and-errors">GitHub</a>.</p>
+    </div>`
+  }
+}
+
 export default class ObsidianFunctionPlot extends Plugin {
   async onload(): Promise<void> {
     this.addCommand({
@@ -28,7 +83,7 @@ export default class ObsidianFunctionPlot extends Plugin {
       editorCallback: (editor: Editor) => {
         new PlotFunctionModal(this.app, (result) => {
           const line = editor.getCursor().line
-          editor.setLine(line, PlotFunctionModal.parseToPlot(result))
+          editor.setLine(line, parseToPlot(result))
         }).open()
       },
     })
@@ -44,54 +99,6 @@ export default class ObsidianFunctionPlot extends Plugin {
     el: HTMLElement,
     _ctx: MarkdownPostProcessorContext
   ): Promise<void> {
-    try {
-      // styles
-      el.classList.add('obsidian-functionplot-render')
-      // parse yaml for bounds and functions to plot
-
-      const header = (source.match(/-{3}[^]+-{3}/) || [null])[0]
-      const functions = (header ? source.substring(header.length) : source)
-        .split('\n')
-        .map((line) => line.trim())
-        .filter((line) => line.length > 0)
-
-      const config: HeaderOptions = Object.assign(
-        {},
-        DEFAULT_HEADER_OPTIONS,
-        header ? parseYaml(header.match(/-{3,}([^]+?)-{3,}/)[1] || '') : {}
-      )
-
-      // prepare options for call to FunctionPlotya
-      const fPlotOptions: FunctionPlotOptions = {
-        target: el,
-        title: config.title,
-        grid: config.grid,
-        disableZoom: config.disableZoom,
-        xAxis: {
-          domain: [config.bounds[0], config.bounds[1]],
-          label: config.xLabel,
-        },
-        yAxis: {
-          domain: [config.bounds[2], config.bounds[3]],
-          label: config.yLabel,
-        },
-        data: functions.map((line) => {
-          return { fn: line.split('=')[1].trim(), graphType: 'polyline' }
-        }),
-      }
-      // render
-      functionPlot(fPlotOptions)
-      // make text listen to stylesheet
-      el.querySelectorAll('text').forEach((el) =>
-        el.setAttribute('fill', 'currentColor')
-      )
-    } catch (e) {
-      el.innerHTML = `
-			<div class="obsidian-functionplot-error">
-				<h3>Error</h3>
-				<p>${e}\n\n${source}</p>
-				<p>Please check your syntax. If this error keeps happening, please file a bug report on <a href="https://github.com/leonhma/obsidian-functionplot#bugs-and-errors">GitHub</a>.</p>
-			</div>`
-    }
+    plotFunction(source, el)
   }
 }
