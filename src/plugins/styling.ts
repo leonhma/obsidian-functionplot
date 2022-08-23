6ba2ab5b8834eebebb5196b6f9c1d366de0f7881
leonhma <leonhardmasche@gmail.com>
2022-08-23 13:22:10 +0200

    feat: styling is applied now

--- src/plugins/styling.ts
+++ src/plugins/styling.ts
@@ -6,13 +6,25 @@ export default function createStylingPlugin(plugin: ObsidianFunctionPlot) {
         if (!instance.listenerCount('after:draw')) {
             instance.on('after:draw', () => {
                 console.log('after:draw')
-                instance.root.select('.title').style('font-size', `${plugin.settings.titleFontSize}px`).style('fill', plugin.settings.fontColor)
-                instance.root.selectAll('.axis-label').style('font-size', `${plugin.settings.labelFontSize}px`).style('fill', plugin.settings.fontColor)
-                instance.root.selectAll('.origin').style('stroke', plugin.settings.lineColor).style('stroke-width', `${plugin.settings.lineWidth}px`).style('opacity', 1)
-                instance.root.selectAll('.tick line').style('stroke', plugin.settings.gridColor).style('stroke-width', `${plugin.settings.gridWidth}px`).style('opacity', 1)
-                instance.root.selectAll('line.text').style('fill', plugin.settings.fontColor).style('font-size', `${plugin.settings.scaleFontSize}px`)
-                instance.root.selectAll('.domain').style('stroke', plugin.settings.lineColor).style('stroke-width', `${plugin.settings.gridWidth}px`).style('opacity', 1)
+                const selection = instance.root.merge(instance.root.enter)
+
+                selection.select('.title')
+                    .style('font-size', `${plugin.settings.titleFontSize}px`)
+                    .style('fill', plugin.settings.fontColor)
+
+                selection.selectAll('.axis-label').style('font-size', `${plugin.settings.labelFontSize}px`).style('fill', plugin.settings.fontColor)
+
+                selection.selectAll('.origin').style('stroke', plugin.settings.lineColor).style('stroke-width', `${plugin.settings.lineWidth}px`).style('opacity', 1)
+
+                selection.selectAll('.tick line').style('stroke', plugin.settings.gridColor).style('stroke-width', `${plugin.settings.gridWidth}px`).style('opacity', 1)
+
+                selection.selectAll('line.text').style('fill', plugin.settings.fontColor).style('font-size', `${plugin.settings.scaleFontSize}px`)
+
+                selection.selectAll('.domain').style('stroke', plugin.settings.gridColor).style('stroke-width', `${plugin.settings.gridWidth}px`).style('opacity', 1)
+
+                instance.root.merge(selection)
             })
+            instance.emit('after:draw')  // self-emit event, remove after mauriciopoppe/function-plot#208
         }
 
     }
